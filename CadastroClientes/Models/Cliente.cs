0b63c1b9fd3c71f281b4168514be1feb5aba1228
nicole.ferreira2 <nicole.ferreira_dasilva@hotmail.com>
2022-08-30 20:15:42 -0300

    Criada Model de Cliente

---
+++ CadastroClientes/Models/Cliente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroClientes.Models
+{
+    public class Cliente
+    {
+        public Cliente(string nome, DateTime nascimento, string email)
+        {
+            Nome = nome;
+            Nascimento = nascimento;
+            Email = email;
+        }
+
+        public int Id { get; private set; }
+
+        [Required(ErrorMessage = "Nome é obrigatório!")]
+        public string Nome { get; private set; }
+        [Required(ErrorMessage = "Nascimento é obrigatório!")]
+        public DateTime Nascimento { get; private set; }
+        [Required(ErrorMessage = "Email é obrigatório!")]
+        public string Email { get; private set; }
+        
+        public int Idade()
+        {
+            int idade = DateTime.Now.Year - Nascimento.Year;
+
+            if (DateTime.Now.DayOfYear < Nascimento.DayOfYear)
+                idade--;
+            return idade;
+        }
+
+        public void AtualizaDados(string nome, DateTime nascimento, string email)
+        {
+            Nome = nome;
+            Nascimento = nascimento;
+            Email = email;
+        }
+    }
+}
