544356f3e3b582f0b806616d4e3dfb036f6b1a2a
nicole.ferreira2 <nicole.ferreira_dasilva@hotmail.com>
2022-08-30 21:36:24 -0300

    Configuração do EFCore

---
+++ CadastroClientes/Migrations/20220831002642_Inicial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CadastroClientes.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nascimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cliente");
+        }
+    }
+}
--- CadastroClientes/Startup.cs
+++ CadastroClientes/Startup.cs
@@ -1,7 +1,9 @@
+using CadastroClientes.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +34,9 @@ namespace CadastroClientes
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CadastroClientes", Version = "v1" });
             });
+
+            services.AddDbContext<AppDbContext>(option =>
+            option.UseSqlServer(Configuration.GetConnectionString("MeuDB")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
